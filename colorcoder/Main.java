29f8391e6b67fd39e97dfea3e54284e711461c00
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-11-20 16:18:05 +0530

    Update Main.java

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -1,4 +1,7 @@
 package colorcoder;
+import colorcoder.MajorColor;
+import colorcoder.MinorColor;
+import colorcoder.ColorPair;
 
 public class Main {
     static void testNumberToPair(int pairNumber,
