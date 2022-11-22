15b4304b7ebfce7c7ae3435e13a059258f28d1e4
Chaitra Laxmeshwar <25486620+ChaitraLaxmeshwar12@users.noreply.github.com>
2022-11-22 22:45:29 +0530

    Update MinorColor.java

--- colorcoder/MinorColor.java
+++ colorcoder/MinorColor.java
@@ -1,12 +1,14 @@
 package colorcoder;
 
-enum MinorColor {
+public enum MinorColor {
     BLUE(0),
     ORANGE(1),
     GREEN(2),
     BROWN(3),
     SLATE(4);
+    
     private int index;
+    
     private MinorColor(int index) {
         this.index = index;
     }
